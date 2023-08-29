fa0ca23a5659374445482260c7b188d99da8fdd2
AnantParmar <adp2002in@gmail.com>
2023-08-29 20:48:30 +0530

    Second Unit Test with moveForward method - failed

--- SpacecraftMovement.java
+++ SpacecraftMovement.java
@@ -4,7 +4,37 @@ public class SpacecraftMovement {
        char direction = 'N';
        char[] commands = {'f', 'r', 'u', 'b', 'l'};
        
+        for(int i=0;i<commands.length;i++) {
+            if(commands[i]=='f') {
+                moveForward(direction, position);
+            }
+        }
+
        System.out.println("Final Position: (" + position[0] + ", " + position[1] + ", " + position[2] + ")");
        System.out.println("Final Direction: " + direction);
+    }
+
+    // moveForward method Implementation
+    public static void moveForward(char direction, int[] position) {
+        switch (direction) {
+            case 'N':
+                position[1]++;
+                break;
+            case 'S':
+                position[1]--;
+                break;
+            case 'E':
+                position[0]++;
+                break;
+            case 'W':
+                position[0]--;
+                break;
+            case 'U':
+                position[2]++;
+                break;
+            case 'D':
+                position[2]--;
+                break;
+        }
     }
 }
