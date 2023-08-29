00231c4db17dd0ce5462c278c408418d4a5aac1d
AnantParmar <adp2002in@gmail.com>
2023-08-29 20:36:32 +0530

    First Unit Test - Failed

---
+++ SpacecraftMovement.java
@@ -0,0 +1,10 @@
+public class SpacecraftMovement {
+    public static void main(String[] args) {
+       int[] position = {0,0,0};
+       char direction = 'N';
+       char[] commands = {'f', 'r', 'u', 'b', 'l'};
+       
+       System.out.println("Final Position: (" + position[0] + ", " + position[1] + ", " + position[2] + ")");
+       System.out.println("Final Direction: " + direction);
+    }
+}
