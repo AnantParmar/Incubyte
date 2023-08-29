e698a32c4826af0ecf484c8f616ba0a80d12a9a2
AnantParmar <adp2002in@gmail.com>
2023-08-29 21:00:22 +0530

    Third Unit Test with moveBackward method - failed

--- SpacecraftMovement.java
+++ SpacecraftMovement.java
@@ -7,6 +7,8 @@ public class SpacecraftMovement {
         for(int i=0;i<commands.length;i++) {
             if(commands[i]=='f') {
                 moveForward(direction, position);
+            } else if(commands[i]=='b') {
+                moveBackward(direction, position);
             }
         }
 
@@ -34,6 +36,30 @@ public class SpacecraftMovement {
                 break;
             case 'D':
                 position[2]--;
+                break;
+        }
+    }
+
+    // moveBackward method Implementation
+    public static void moveBackward(char direction, int[] position) {
+        switch (direction) {
+            case 'N':
+                position[1]--;
+                break;
+            case 'S':
+                position[1]++;
+                break;
+            case 'E':
+                position[0]--;
+                break;
+            case 'W':
+                position[0]++;
+                break;
+            case 'U':
+                position[2]--;
+                break;
+            case 'D':
+                position[2]++;
                 break;
         }
     }
